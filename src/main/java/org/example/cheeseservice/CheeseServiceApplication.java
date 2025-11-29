1e3857487325c366b6463ad7aad418c0f0dba897
Várbíró Nándor <varbiro.nandor@gmail.com>
2025-11-29 12:48:07 +0100

    project init

---
+++ src/main/java/org/example/cheeseservice/CheeseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.cheeseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheeseServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cheeseservice/CheeseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cheeseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
